77bcddca13ab127859e616bbdcb1fc9e7c532f0f
Ernesto Chero <ernesto@jemstep.com>
2020-01-23 17:01:46 -0500

    Review TypeDataSet of frameless version 2

--- src/main/scala/TypedDatasetOverview.scala
+++ src/main/scala/TypedDatasetOverview.scala
@@ -2,7 +2,8 @@ package framelessOverview
 
 import org.apache.spark.{ SparkConf, SparkContext }
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions._
+import org.apache.spark.sql.functions.col
+import frameless.functions._
 import frameless.functions.aggregate._
 import frameless.TypedDataset
 import frameless.syntax._
@@ -20,23 +21,36 @@ object TypedDatasetOverview extends App {
   spark.sparkContext.setLogLevel("WARN")
   import spark.implicits._
   // "LocationID","Borough","Zone","service_zone"
-  case class TaxiZone(LocationId: Int, Borough: String, Zone: String, ServiceZone: String)
+  case class TaxiZone(locationId: Int, borough: String, zone: String, serviceZone: String)
   val taxiZoneDataFrame = spark.read
     .option("header", "true")
     .option("inferSchema", "true")
     .csv("src/main/resources/data/taxi_zones.csv")
 
   val taxiZoneDataSet = taxiZoneDataFrame
-    .select(col("LocationID"), col("Borough"), col("Zone"), col("service_zone").as("ServiceZone"))
+    .select(col("LocationID").as("locationId"),
+            col("Borough").as("borough"),
+            col("Zone").as("zone"),
+            col("service_zone").as("serviceZone"))
     .as[TaxiZone]
 
-  val taxiZoneTypedDataSet: TypedDataset[TaxiZone] = TypedDataset.create(taxiZoneDataSet)
+  val tzTypedDS: TypedDataset[TaxiZone] = TypedDataset.create(taxiZoneDataSet)
   val zonesTyped = for {
-    count <- taxiZoneTypedDataSet.count()
-    zones <- taxiZoneTypedDataSet.take((count / 2).toInt)
-  } yield zones
-
+    count <- tzTypedDS.count()
+    zones <- tzTypedDS.take((count / 2).toInt)
+    zonesUpperCase = zones.map(tz => tz.copy(zone = tz.zone.toUpperCase))
+  } yield zonesUpperCase
   zonesTyped.run().foreach(println(_))
+
+  case class BoroughWithZone(borough: String, zone: String, random: Int)
+
+  val resultTyped: TypedDataset[BoroughWithZone] =
+    tzTypedDS
+      .select(tzTypedDS('borough),
+              nonAggregate.concat(tzTypedDS('locationId).cast[String], tzTypedDS('zone)),
+              frameless.functions.lit(1000))
+      .as[BoroughWithZone]()
+  resultTyped.show().run()
 
   case class Bar(d: Double, s: String)
   case class Foo(i: Int, b: Bar)
@@ -51,5 +65,44 @@ object TypedDatasetOverview extends App {
     Apartment("Lyon", 45, 133000.0, 1),
     Apartment("Nice", 74, 325000.0, 3)
   )
+  val aptds: TypedDataset[Apartment] = TypedDataset.create(apartments)
 
+  case class ApartmentDetails(city: String, price: Double, surface: Int, ratio: Double)
+  val aptWithRatio = aptds
+    .select(
+      aptds('city),
+      aptds('price),
+      aptds('surface),
+      aptds('price) / aptds('surface).cast[Double]
+    )
+    .as[ApartmentDetails]()
+
+  case class CityRatio(city: String, ratio: Double)
+  val cityRatio: TypedDataset[CityRatio] = aptWithRatio.project[CityRatio]
+  cityRatio.show().run()
+
+  case class PriceInfo(ratio: Double, price: Double)
+  val priceInfo: TypedDataset[PriceInfo] = aptWithRatio.project[PriceInfo]
+  priceInfo.show().run()
+
+  case class ApartmentShortInfo(city: String, price: Double, bedrooms: Int)
+  val aptTypedDs2: TypedDataset[ApartmentShortInfo] = aptds.project[ApartmentShortInfo]
+  aptTypedDs2.union(aptds).show().run()
+
+  aptTypedDs2
+    .withColumnTupled(
+      frameless.functions.nonAggregate
+        .when(aptTypedDs2('city) === "Paris", aptTypedDs2('price))
+        .when(aptTypedDs2('city) === "Lyon", frameless.functions.lit(1.1))
+        .otherwise(frameless.functions.lit(0.0))
+    )
+    .show()
+    .run()
+
+  val t = cityRatio.select(cityRatio('city), lit(List("abc", "c", "d")))
+  t.withColumnTupled(
+      frameless.functions.nonAggregate.arrayContains(t('_2), " abc")
+    )
+    .show()
+    .run()
 }
